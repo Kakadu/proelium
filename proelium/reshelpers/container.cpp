9154fb4709cff93460bf3eef79e7c429c3ab10fc
kakadu <kakadu.hafanana@gmail.com>
2011-05-29 15:26:41 +0400

    forgotten files

--- proelium/reshelpers/container.cpp
+++ proelium/reshelpers/container.cpp
@@ -4,15 +4,4 @@
 QMap<QString, SpritesPack*> Sprites;
 UserActionHyperVisor* MainHyperVisor;
 
-QPair<int,int> directions[10] = {
-    QPair<int,int>( 0, 0),  // unused
-    QPair<int,int>(-1, 0),  //SW
-    QPair<int,int>(-1, 1),  //S
-    QPair<int,int>( 0, 1),  //SE
-    QPair<int,int>(-1,-1),  //W
-    QPair<int,int>( 0, 0),  // unused
-    QPair<int,int>( 1, 1),  //E
-    QPair<int,int>( 0,-1),  //NW
-    QPair<int,int>( 1,-1),  //N
-    QPair<int,int>( 1, 0)   //NE
-};
+
